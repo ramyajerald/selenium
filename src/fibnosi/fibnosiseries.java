7a8f941d0538fe3ce8ad7eef1e308a36cf2f8493
Dell <Dell@DESKTOP-KJ2LKUS>
2019-02-22 09:27:54 +0530

    Test

---
+++ src/fibnosi/fibnosiseries.java
@@ -0,0 +1,22 @@
+package fibnosi;
+
+public class fibnosiseries {
+	
+	public static void main(String args[]){
+	
+	int i;
+	int a1=1;
+	int a2=0;
+	int sum;
+	
+	for (i=0; i<=10;i++)
+	{
+		System.out.println("the value of sum is :"+a2);
+		sum=a1+a2;
+		a1=a2;
+		a2=sum;	
+	}
+	
+}
+
+}
