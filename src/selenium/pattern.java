63807aab1988e4d9ba41beb6d42cfd181c000faf
Dell <Dell@DESKTOP-KJ2LKUS>
2019-02-22 09:31:16 +0530

    Test

---
+++ src/selenium/pattern.java
@@ -0,0 +1,14 @@
+package selenium;
+
+public class pattern{
+	
+	public static void main(String[] args) {
+	
+for (int a=5;a>=1;a--) {
+	for (int b=a;b>=1;b--){ 
+	System.out.print(b);  
+	} 
+	System.out.println();
+}
+}
+}
